249ad6238d0773152ac97b57c1f05be18ac974c5
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-03 09:27:47 -0700

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(input_string):
         if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
         print("{}".format(char), end="")
-    print("")    
+    print("")
