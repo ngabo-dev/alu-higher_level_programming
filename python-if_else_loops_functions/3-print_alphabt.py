b3272402e02f6b4e633dc914e03b895a2e73b1b8
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-03 02:40:21 -0700

    print alphabet except a & q

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-alphabet = ''.join(chr(char) for char in range(ord('a'), ord('z') + 1) 
-if chr(char) not in ['e', 'q'])
-print("{}".format(alphabet), end='')
+alphabet = ''.join(chr(char) for char in range(ord('a'), ord('z') + 1)
+    if chr(char) not in ['e', 'q'])
+    print("{}".format(alphabet), end='')
