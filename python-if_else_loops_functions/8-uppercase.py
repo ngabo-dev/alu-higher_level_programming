7e6f4c99aec0e65c5084ae121d92a136104444ce
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-03 09:26:30 -0700

    task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,21 +2,7 @@
 def uppercase(input_string):
     output_string = ""
     for char in input_string:
-        if 97 <= ord(char) <= 122:
+        if ord(char) >= 97 and ord(char) <= 122:
             char = chr(ord(char) - 32)
-        output_string += char
-    print("{}\n".format(output_string))
-
-#test cases
-test_cases = [
-    "holberton",
-    "Holberton School",
-    "Holberton School, 98 battery street",
-    "",
-    "98",
-    "z"
-]
-
-for test_case in test_cases:
-    print("Input: {}".format(test_case))
-    uppercase(test_case)
+        print("{}".format(char), end="")
+    print("")    
