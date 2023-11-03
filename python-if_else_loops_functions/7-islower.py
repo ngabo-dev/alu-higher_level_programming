241340cc6b015c1483f2ead2f41c25e9779363e4
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-03 09:12:57 -0700

    task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
 def islower(c):
-    return 97 <= ord(c) <= 122
-
-character = input("")
-if islower(character):
-    print(f"{character} => lower")
-else:
-    print(f"{character} => upper")
+    if ord(c) >= 97 and ord(c) <= 122
+        return True
+    else:
+        return False
