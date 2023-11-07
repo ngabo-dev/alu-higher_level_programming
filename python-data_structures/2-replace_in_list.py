354528f08657c65b1636d96ab9050e9ca208155a
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-07 08:36:11 -0800

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,3 +2,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
+    else:
+        new_list = my_list[:idx] + [element] + my_list[idx + 1:]
+        return new_list
