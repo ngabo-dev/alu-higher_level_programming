4d46fde9cbaa8281944c0e336e01435651f79e64
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-07 08:41:04 -0800

    task 2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,5 +3,6 @@ def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_list = my_list[:idx] + [element] + my_list[idx + 1:]
+        new_list = my_list[:]
+        new_list[idx] = element
         return new_list
