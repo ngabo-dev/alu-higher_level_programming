d36cfe838f4b79a1cdcf61d317aa5683a39237b5
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-08 07:30:45 -0800

    task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-        if idx < 0 and idx >= len(my_list):
-            return my_list[:]
+    if idx < 0 and idx >= len(my_list):
+        return my_list[:]
 
-        list_storage = my_list[:]
-        list_storage[idx] = element
-        return list_storage
+    list_storage = my_list[:]
+    list_storage[idx] = element
+    return list_storage
