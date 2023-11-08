87406a30fbd52dd750be52389cde39d7e1f276ba
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-08 08:38:58 -0800

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_new_string = my_string.translate({ ord("c")})
+    my_new_string = my_string.translate( { ord("c"): None } )
     print(my_new_string)
