4f49335ac2716b516ba81f73e5adfbb307de44ad
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 07:19:13 -0800

    replacing task 2

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for search in my_list:
-        my_list[search] = replace
+        my_list(search[0]) = replace
         return my_list
     else:
         return my_list
