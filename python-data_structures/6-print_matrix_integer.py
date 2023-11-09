6b77c75606ec3f85682dea25efd7a510ac69b8cd
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 05:20:27 -0800

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+        formated_row = " ".join("{:d}".format(num) for num in row)
+        print(formated_row)
