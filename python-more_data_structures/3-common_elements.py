aee16dfe6606f53bc84250943d315c0909d18515
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 08:15:08 -0800

    common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-# new set to store common elements
+    # new set to store common elements
     same_elements = set()
     for element in set_1:
         if element in set_2:
