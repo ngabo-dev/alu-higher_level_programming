f3a663efa5e51e4580e5331be0b36dd0a5af05ee
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 05:00:31 -0800

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,9 +3,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    new_list = my_list[:idx] + my_list[idx+1:]
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
     return new_list
-
-original_list = [1, 2, 3, 4, 5, 6, 7]
-modified_list = delete_at(original_list, 4)
-print(modified_list)
