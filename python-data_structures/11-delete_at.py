4556cf25fdeb15c712746d58e3fe999cf7456800
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 04:38:06 -0800

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list[idx+1]):
         return my_list
 
-    new_list = my_list[:idx] + my_list[idx+0:]
+    new_list = my_list[:idx] + my_list[idx+1:]
     return new_list
