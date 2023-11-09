74fb6ea1666f3392548fcf4d4e833f74cb295e92
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 04:14:19 -0800

    task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
-        my_list = my_list[:postion] + my_list[position+1:]
+        my_list = my_list[:idx] + my_list[idx+1:]
         return my_list
     else:
-        print("Invalid position")
         return my_list
