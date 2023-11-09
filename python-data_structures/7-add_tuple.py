58930557502554e31127fccd8433f1ca959a3b54
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 03:33:42 -0800

    task 6

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a[:2] + (0, 0)[:2-len(tuple_a)
-    b = tuple_b[:2] + (0, 0)[:2-len(tuple_b)]
-    result = (a[0] + b[0], a[1] + b[1])
-    return result
+    if len(tuple_a) == 0:
+        a, b = (0, 0)
+    elif len(tuple_a) == 1:
+        a = tuple_a[0]
+        b = 0
+    else:
+        a = tuple_a[0]
+        b = tuple_a[1]
+    if len(tuple_b) == 0:
+        c, d = (0, 0)
+    elif len(tuple_b) == 1:
+        c = tuple_a[0]
+        d = 0
+    else:
+        c = tuple_b[0]
+        d = tuple_b[1]
+    new_tuple = (a + c, b + d)
+    return(new_tuple)
