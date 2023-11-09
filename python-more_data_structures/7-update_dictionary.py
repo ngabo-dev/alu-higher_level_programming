b3a9bc2a4e4dd90db06fac40a08ab9b58be5e448
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 09:10:22 -0800

    update dictionary

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    updated_dict = []
-    for item in a_dictionary:
-        if item in a_dictionary:
-            a_dictionary[item] == key
-            update_dict = value
-    return update_dict
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
+    return a_dictionary
