01d02b4c430895bdd66239df8ba99339e995a7e8
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 07:15:56 -0800

    replacing task 2

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    for search in my_list:
+        my_list[0] = replace
+        return my_list
+    else:
+        return my_list
