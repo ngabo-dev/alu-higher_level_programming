62fba817d0ba2875e8f0f3070b7e48f9a3e60596
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 08:36:15 -0800

    different elements

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,8 +3,8 @@ def only_diff_elements(set_1, set_2):
     set_3 = set()
     for element in set_1:
         if element not in set_2:
-            set_3.append(element)
+            set_3.add(element)
     for element in set_2:
         if element not in set_1:
-            set_3.append(element)
+            set_3.add(element)
     return set_3
