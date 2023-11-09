bf79e51d74fc55224d1cc3f1d7e81f145c0d7860
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 09:05:03 -0800

    update

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    updated_dict = []
+    for item in a_dictionary:
+        if item in a_dictionary:
+            a_dictionary[item] == key
+            update_dict = value
+    return update_dict
