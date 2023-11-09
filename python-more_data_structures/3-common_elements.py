e5eec603da8978738577c84c6dcb49e234cb5603
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-09 08:09:04 -0800

    common elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    same_elements = []
+    for element in set_1:
+        if element in set_2:
+            same_elements.add(element)
+    return same_elements
