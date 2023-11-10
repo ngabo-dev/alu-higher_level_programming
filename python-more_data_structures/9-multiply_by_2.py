6af305b5d0a5ac0ec1e36ebdb52dd8b6a5c86f81
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-10 00:10:42 -0800

    multiple

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,5 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_dict = 1
     for keys in a_dictionary:
-        new_dict = (keys ** 2)
-    return new_dict    
+        new_dict = (int(keys) ** 2)
+    return new_dict
