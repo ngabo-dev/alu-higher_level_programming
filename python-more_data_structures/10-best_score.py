1a2308a08076a171e78095fb52a88c139b062cad
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-10 03:45:45 -0800

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+
+
 def best_score(a_dictionary):
-    best_score_key = None
-    best_score = None
-    for key, value in a_dictionary.items():
-        if isinstance(value, int) and (best_score is None or value > best_score):
-            best_score = value
-            best_score_key = key
-    return best_score_key
+    if a_dictionary:
+        return max(a_dictionary, key=a_dictionary.get)
