d0d9ff2016be3cee095138a52db6ea181291ba9b
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-16 08:42:26 -0800

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""no module to import"""
+
+
 class Square:
-    """
-    This class represents a square.
-    """
+    """This class represents a square."""
 
     def __init__(self, size=0):
         """
