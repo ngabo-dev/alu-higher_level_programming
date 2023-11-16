f92bc759bf8ac4857d161fe1a36891737669de21
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-16 04:40:50 -0800

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+"""Initializes a new instance of the Square class."""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
