8c7618e83d3d0bd5aa8e5f1f0104c2e9e9c53ba1
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-20 06:46:08 -0800

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" defining class to inherit from list"""
+"""
+Module 1-my_list
+Contains class MyList
+inherits from list; has public instance method to print sorted
+"""
 
 
 class MyList(list):
+    """inherits from list
+    methods:
+    print_sorted(self)
     """
-    A custom list class that inherits from the built-in.
-    """
-
     def print_sorted(self):
-        """
-        Prints the list in ascending sorted order.
-        """
-        new_list = self.copy()
-        new_list.sort()
-        print(new_list)
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
