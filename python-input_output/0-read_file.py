54f3ac4780bd5bc4f78bce299df2cde7f3b8d59f
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-24 07:35:07 -0800

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-"""module to display an empty class"""
+"""
+function that reads a text file
+"""
 
 
 def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
     """
-    Reads a text file and prints its content to stdout.
-
-    Parameters:
-    - filename (str): The name of the text file to be read. Defaults to an empty string.
-
-    Returns:
-    - None
-    """
-    if not filename:
-        print("Error: Please provide a filename.")
-        return
-
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except FileNotFoundError:
-        print(f"Error: File '{filename}' not found.")
-    except Exception as e:
-        print(f"Error: An unexpected error occurred - {e}")
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
