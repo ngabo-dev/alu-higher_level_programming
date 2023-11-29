6654ded5c709d7a0971af78b84d97723c1ba61b4
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-29 02:38:02 -0800

    full table description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- printing full description of a table in the database
+
+SELECT VARCHAR, INT, 256, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = mysql AND TABLE_NAME = first_table;
+
