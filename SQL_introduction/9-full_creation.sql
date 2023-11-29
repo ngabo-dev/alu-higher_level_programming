1dc3e781e978765aaef4307703d5604152106cf1
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-29 04:41:47 -0800

    creating second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- creating a second table in the database
 
-CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR, score INT);
+CREATE TABLE second_table(id INT, name VARCHAR, score INT);
 INSERT INTO second_table
 VALUES
-('1', 'John', '10')
-('2', 'Alex', '3')
-('3', 'Bob', '14')
-('4', 'George', '8')
+('1', 'John', '10');
+('2', 'Alex', '3');
+('3', 'Bob', '14');
+('4', 'George', '8');
