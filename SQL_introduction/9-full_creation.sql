512d3058b0a8be7ec6cc00f3cab5a97caf370e40
ngabo-dev <j.niyongabo@alustudent.com>
2023-11-29 04:46:30 -0800

    creating second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,7 +3,7 @@
 CREATE TABLE second_table(id INT, name VARCHAR, score INT);
 INSERT INTO second_table
 VALUES
-('1', 'John', '10');
-('2', 'Alex', '3');
-('3', 'Bob', '14');
-('4', 'George', '8');
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
