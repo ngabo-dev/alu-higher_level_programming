5e4e38da161f1af42f625470042b670faa502379
ngabo-dev <j.niyongabo@alustudent.com>
2023-12-01 05:54:28 -0800

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-"""
-script that fetches https://alu-intranet.hbtn.io/status
-using the urllib package.
-"""
-import urllib.request
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
 
-if __name__ == '__main__':
-    url = 'https://alu-intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("utf-8")))
-
-        # Check if the content matches the expected value
-        expected_content = b'Custom status'
-        if content == expected_content:
-            print("Success! Content matches the expected value.")
-        else:
-            print("Warning! Content doesn't match the expected value.")
-            print("\t- Expected: {}".format(expected_content))
-            print("\t- Actual: {}".format(content))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
