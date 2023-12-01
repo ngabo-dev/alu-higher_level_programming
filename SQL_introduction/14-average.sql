335c694477b5423c5df550d8c6f21499dfae9623
ngabo-dev <j.niyongabo@alustudent.com>
2023-12-01 03:17:17 -0800

    task 14

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,5 @@
--- calculating the average score
--- The result column name should be average
+-- a script that computes the score average of all records in the table second_table of the database hbtn_0c_0
+-- the result shoul be the average
 
-SELECT AVG('score') AS 'average' FROM 'second_table';
+SELECT AVG(`score`) AS `average` FROM `second_table`;
+
