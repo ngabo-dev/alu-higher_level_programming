0fb1ccb2e24c55019f77b3952c1ded00aa76b205
ngabo-dev <j.niyongabo@alustudent.com>
2023-12-13 04:37:04 -0800

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,4 +9,4 @@ def text_indentation(text):
     text = text.replace(".", ".\n\n")
     text = text.replace("?", "?\n\n")
     text = text.replace(":", ":\n\n")
-    print(text)
+    print(text, end="")
