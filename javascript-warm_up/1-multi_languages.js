c218a1c440e5a999ebfc55b0722613f94b844ec6
ngabo-dev <j.niyongabo@alustudent.com>
2024-01-31 05:46:44 -0800

    3 languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-let languages = 'C is fun', 'Python is cool', 'JavaScript is amazing';
+let languages = 'C is fun';
+let languages = 'Python is cool';
+let languages = 'JavaScript is amazing';
 console.log(languages);
