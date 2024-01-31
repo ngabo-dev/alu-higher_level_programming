986940c7466c1c3d0ae2a161aea0e04671b44e1c
ngabo-dev <j.niyongabo@alustudent.com>
2024-01-31 08:35:31 -0800

    creating a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const firstArgument = process.argv[2];
 const secondArgument = process.argv[3];
-console.log('${firstArgument} is ${secondArgument}')
+console.log(firstArgument + ' is ' + secondArgument);
