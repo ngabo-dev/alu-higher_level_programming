a4a9329102cdedb42365f970adeeddc314f234e3
ngabo-dev <j.niyongabo@alustudent.com>
2024-01-31 06:26:06 -0800

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const numArguments = process.argv.length -2;
+const numArguments = process.argv.length - 2;
 
 if (numArguments === 0) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (numArguments === 1) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
