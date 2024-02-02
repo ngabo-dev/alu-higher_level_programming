b451d93d6452c1dd9a0e70001d8cafb5aae59daf
ngabo-dev <j.niyongabo@alustudent.com>
2024-02-02 04:09:25 -0800

    add file

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
-}
-
-const add = function(a, b) {
+const add = function (a, b) {
   return a + b;
 };
-const add = (a, b) => a + b;
+
+console.log(add(3, 5));
+console.log(add(-2, 7));
