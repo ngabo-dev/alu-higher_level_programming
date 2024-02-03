8a58c5e975c37e6887fa08f3fbc5367034bb0aa6
ngabo-dev <j.niyongabo@alustudent.com>
2024-02-03 04:53:38 -0800

    rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
+    // Check if w and h are positive integers
     if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)) {
+      // Initialize the instance attributes width and height
       this.width = w;
       this.height = h;
     } else {
+      // Create an instance with width and height set to undefined
       return new Rectangle();
     }
   }
 }
+
 module.exports = Rectangle;
