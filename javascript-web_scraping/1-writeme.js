87320093a361c0bcb48080ff66c2169e3d83fa4f
ngabo-dev <j.niyongabo@alustudent.com>
2024-02-08 04:00:23 -0800

    writing a string to a file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+function writeToFile (filePath, content) {
+  fs.writeFile(filePath, content, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Content has been written to the file successfully.');
+  });
+}
+
+if (process.argv.length !== 4) {
+  console.log('Usage: node script.js <file_path> <content>');
+} else {
+  const filePath = process.argv[2];
+  const content = process.argv[3];
+  writeToFile(filePath, content);
+}
