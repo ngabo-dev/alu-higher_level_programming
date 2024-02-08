583d9d23cfbd9c4727d664d313443d8d04e209d6
ngabo-dev <j.niyongabo@alustudent.com>
2024-02-08 05:17:59 -0800

    count movies

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,47 +1,28 @@
 #!/usr/bin/node
 const request = require('request');
 
-function getNumberOfMoviesWithCharacter (apiUrl, characterId) {
-  let totalMovies = 0;
-
-  function fetchMovies (url) {
-    request.get(url, (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      if (response.statusCode !== 200) {
-        console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
-        return;
-      }
-
-      const data = JSON.parse(body);
-      const movies = data.results;
-
-      // Check if Wedge Antilles is present in each movie
-      movies.forEach(movie => {
-        if (movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-          totalMovies++;
+function getNumberOfMoviesWithCharacter(apiUrl, characterId) {
+    request.get(apiUrl, (error, response, body) => {
+        if (error) {
+            console.error(error);
+            return;
         }
-      });
 
-      // Check if there are more pages to fetch
-      if (data.next) {
-        fetchMovies(data.next);
-      } else {
-        console.log(`${totalMovies}`);
-      }
+        if (response.statusCode !== 200) {
+            console.error(`Failed to fetch movie data. Status code: ${response.statusCode}`);
+            return;
+        }
+
+        const films = JSON.parse(body).results;
+        const moviesWithCharacter = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`));
+        console.log(`${moviesWithCharacter.length}`);
     });
-  }
-
-  fetchMovies(apiUrl);
 }
 
 if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <api_url>');
+    console.log('Usage: node script.js <api_url>');
 } else {
-  const apiUrl = process.argv[2];
-  const characterId = 18; // Character ID for Wedge Antilles
-  getNumberOfMoviesWithCharacter(apiUrl, characterId);
+    const apiUrl = process.argv[2];
+    const characterId = 18; // Character ID for Wedge Antilles
+    getNumberOfMoviesWithCharacter(apiUrl, characterId);
 }
