c371e2009e84378edfa31ed7845fc7f3d95a6cbd
ngabo-dev <j.niyongabo@alustudent.com>
2024-02-11 12:22:25 -0800

    star wars wedge

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,35 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-function countMoviesWithCharacter (apiUrl, characterId) {
-  request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    if (response.statusCode !== 200) {
-      console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-      return;
-    }
-
-    const films = JSON.parse(body).results;
-    let count = 0;
-
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-
-    console.log(count);
-  });
-}
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node script.js <api_url>');
-} else {
-  const apiUrl = process.argv[2];
-  const characterId = 18;
-  countMoviesWithCharacter(apiUrl, characterId);
-}
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});
